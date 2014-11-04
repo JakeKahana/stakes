045bce680c29aa8683c1e10d0eb66b619597e263
JakeKahana <jakekahana@gmail.com>
2014-11-03 20:13:38 -0500

    Trouble with Collections

--- client/client.js
+++ client/client.js
@@ -1,66 +1,73 @@
 Meteor.startup(function () {
 
-//loading animation fades after 1/2 seconds
-  $("#loading").delay(2500).slideUp(100);
-  $("#randoms").delay(2600).slideDown(100, function(){
-      $("#header").removeClass('hidden');
-      $("#button").removeClass('hidden');
-      $("#randoms").removeClass('hidden');
-      $("#header").addClass('shown');
-      $("#button").addClass('shown');
-      $("#randoms").addClass('shown');
-  });   
+  //loading animation fades after 1/2 seconds
+    $("#loading").delay(250).slideUp(100);
+    $("#randoms").delay(260).slideDown(100, function(){
+        $("#header").removeClass('hidden');
+        $("#button").removeClass('hidden');
+        $("#randoms").removeClass('hidden');
+        $("#header").addClass('shown');
+        $("#button").addClass('shown');
+        $("#randoms").addClass('shown');
+    });   
 });
 
-Meteor.subscribe("allpunishments");
-Meteor.subscribe("allbets");
+  //generate random number on die
+    Template.header.dienumber =function(){
+      var randomdie = Math.floor((Math.random() * 6)+1);
+      return "../images/die"+randomdie+".gif"
+    }
 
-//generate random number on die
-  Template.header.dienumber =function(){
-    var randomdie = Math.floor((Math.random() * 6)+1);
-    return "../images/die"+randomdie+".gif"
-  }
+  //click on "Start new bet"
+  // Template.randoms.events({
+  //   'click #button': function(){
+  //     $('#randoms').fadeOut(300, function(){
+  //       $("#createbet").fadeIn(300, function(){
+  //         $("#createbet").removeClass('hidden');
+  //       });
+  //     });
+  //    } 
+  // });
 
-//click on "Start new bet"
-Template.randoms.events({
-  'click #button': function(){
-    $('#randoms').fadeOut(300, function(){
-      $("#createbet").fadeIn(300, function(){
-        $("#createbet").removeClass('hidden');
-      });
+  if(Meteor.isClient){
+    Meteor.subscribe('actions');
+  Template.randoms.helpers({
+      thestake: function(){
+        return Punishments.find({id: "xy5iYirB8CaTDi5EY"});
+      }
     });
-   } 
-});
 
-//click on logo, make all templates disappear then random reappears
-Template.header.events({
-  'click .logo': function(){
-    $("#createbet").fadeOut(100, function(){
-      $("#randoms").fadeOut(100, function(){
-        $("#dashboard").fadeOut(100, function(){
+
+  //click on logo, make all templates disappear then random reappears
+  Template.header.events({
+    'click .logo': function(){
+      $("#createbet").fadeOut(100, function(){
+        $("#randoms").fadeOut(100, function(){
+          $("#dashboard").fadeOut(100, function(){
+          });
         });
       });
-    });
-    $("#randoms").removeClass('hidden');
-    $("#randoms").fadeIn(300);
+      $("#randoms").removeClass('hidden');
+      $("#randoms").fadeIn(300);
 
-    }
-});
-
-//Change stakes when you click (NEED TO SWIPE)
-Template.randoms.events({
-  'click #stakes': function () {
+      }
+  });
 
 
-    $("#directions").fadeOut(450, function(){
-      $("#dare").fadeOut(400, function(){
-        var randomnum = Math.floor((Math.random() * punishments.length));
-        $("#dare").html(punishments[randomnum]);
-        $("#dare").fadeIn(200);
-      });
-    });
-  }
-}); 
-
-
+  // //Change stakes when you click (NEED TO SWIPE)
+  // Template.randoms.events({
+  //   'click #stakes': function () {
+  //     $("#directions").fadeOut(450, function(){
+  //       $("#dare").fadeOut(400, function(){
+  //           Template.randoms.dare = function(){
+  //             return Punishments.find();
+  //           }
+  //         //var randompunishment = Punishments.findOne({}, {sort: limit: 1})
+  //         //$("#dare").html(randompunishment);
+  //         $("#dare").fadeIn(200);
+  //       });
+  //     });
+  //   }
+  //}); 
+};
 
--- server/data.js
+++ server/data.js
@@ -1,12 +1,10 @@
-var punishments = [
-  "Have The Winner Sneeze in the Loser's face",
-  "Go to a WNBA Game Alone without Your Phone",
-  "Drink 2 Five Hour Energy Drinks within an Hour",
-  "Eat a Dog Biscuit",
-  "Text Your Most Recent Ex",
-  "Be the DD for the entire next weekend",
-  "Send a nude selfie to the person the winner chooses",
-  "Wax their junk entirely",
-  "Make an announcement on the train",
-  "Kiss a stranger",
-  ];
+//Punishments.insert({action: "Have The Winner Sneeze in the Loser's Face"});
+  // "Go to a WNBA Game Alone without Your Phone",
+  // "Drink 2 Five Hour Energy Drinks within an Hour",
+  // "Eat a Dog Biscuit",
+  // "Text Your Most Recent Ex",
+  // "Be the DD for the entire next weekend",
+  // "Send a nude selfie to the person the winner chooses",
+  // "Wax their junk entirely",
+  // "Make an announcement on the train",
+  // "Kiss a stranger",
